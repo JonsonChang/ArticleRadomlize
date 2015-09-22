a26c5d4553be7a296ba51e292d892097dc012117
Jonson Chang <jonsonchang@ingrasys.com>
2015-09-22 18:08:31 +0800

    improve

--- main.py
+++ main.py
@@ -3,61 +3,69 @@
 import random
 import codecs
 
-fin = open('input.txt', 'r')
-article = fin.read()
+
+class article_rnd:
+    infile = ""
+    outfile = ""
+
+    def __init__(self, infile_name, outfile_name):
+        self.infile = infile_name
+        self.outfile = outfile_name
+        fin = open(infile_name, 'r')
+        self.article = fin.read()
+
+    def my_split(self, str, c):
+        tmp = str.split(c)
+        return tmp
+
+    def list_split(self, alist, c):
+        tmp = []
+        for line in alist:
+            tmp.extend(self.my_split(line, c))
+        return tmp
+
+    def randomlize(self, alist):
+        tmp = []
+        blist = alist
+        while len(blist) > 0:
+            lenght = len(blist)
+            idx = random.randint(0, lenght - 1)
+            tmp.append(blist[idx])
+            blist.remove(blist[idx])
+
+        return tmp
+
+    def output(self, alist):
+        fout = codecs.open(self.outfile, "w", 'utf-8')
+        newSection = True
+        for aline in alist:
+            line = aline.strip()
+            if (len(line)) < 3:
+                if newSection != True:
+                    fout.write(u"。\r\n")
+                else:
+                    fout.write(u"\r\n")
+                newSection = True
+            else:
+                if newSection != True:
+                    fout.write(u"，")
+                fout.write(line)
+                newSection = False
+        fout.close()
+
+    def process(self):
+        step1 = self.my_split(self.article.decode('utf-8'), u'，')
+        step1 = self.list_split(step1, u"\n")
+        step1 = self.list_split(step1, u',')
+        step1 = self.list_split(step1, u'。')
+        step1 = self.list_split(step1, u'；')
+        step1 = self.list_split(step1, u'?')
+        step1 = self.list_split(step1, u' ')
+
+        done = self.randomlize(step1)
+        self.output(done)
 
 
-def my_split(str, c):
-    tmp = str.split(c)
-    return tmp
+a = article_rnd("input.txt", "output.txt")
+a.process()
 
-
-def list_split(alist, c):
-    tmp = []
-    for line in alist:
-        tmp.extend(my_split(line, c))
-    return tmp
-
-
-def randomlize(alist):
-    tmp = []
-    blist = alist
-    while len(blist) > 0:
-        lenght = len(blist)
-        idx = random.randint(0, lenght - 1)
-        tmp.append(blist[idx])
-        blist.remove(blist[idx])
-
-    return tmp
-
-
-def output(alist):
-    fout = codecs.open("output.txt", "w", 'utf-8')
-    newSection = True
-    for aline in alist:
-        line = aline.strip()
-        print len(line)
-        if (len(line)) < 1:
-            fout.write(u"。\r\n")
-            newSection = True
-        else:
-            if newSection != True:
-                fout.write(u"，")
-            fout.write(line)
-            newSection = False
-    fout.close()
-
-
-step1 = my_split(article.decode('utf-8'), u'，')
-step1 = list_split(step1, u',')
-step1 = list_split(step1, u'。')
-step1 = list_split(step1, u'；')
-step1 = list_split(step1, u'?')
-step1 = list_split(step1, u' ')
-
-done = randomlize(step1)
-output(done)
-
-# for line in done:
-#     print "--"
-#     print line
